57ad6902610adf37734c8efcc2f8cddf9475ff0b
ransomware <ayana.percey@gmail.com>
2023-12-23 15:01:15 +0100

    sans limits

--- ft_substr.c
+++ ft_substr.c
@@ -19,26 +19,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*new;
-	unsigned int	i;
+	char	*substr;
+	size_t	s_len;
 
-	if (s == NULL)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		len = 0;
-	else if (len > ft_strlen(s + start))
-		len = ft_strlen(s + start);
-	if (len > SIZE_MAX - 1)
-		return (NULL);
-	new = malloc(sizeof(char) * (len + 1));
-	if (!new)
-		return (NULL);
-	i = 0;
-	while (start + i < ft_strlen(s) && i < len)
+	s_len = ft_strlen(s);
+	if (start >= s_len)
 	{
-		new[i] = s[start + i];
-		i++;
+		substr = malloc(1 * sizeof(char));
+		if (!substr)
+			return (NULL);
+		*substr = '\0';
+		return (substr);
 	}
-	new[i] = '\0';
-	return (new);
+	if (len > s_len)
+		len = s_len;
+	if (len > s_len - start)
+		len = s_len - start;
+	substr = malloc(1 + len * sizeof(char));
+	if (substr == NULL)
+		return (NULL);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
 }
